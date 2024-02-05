dcde6ae1d2e23b99ecd2bd2dffa1ceb849c7f72c
n1220205 <n1220205@PC1220205.jn2.iwasaki.ac.jp>
2024-02-05 11:28:28 +0900

    練習02_2

---
+++ SampleSQL/src/main/java/jp/ac/isc/cloud/UserUpdateServlet.java
@@ -0,0 +1,46 @@
+package jp.ac.isc.cloud;
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class UserUpdateServlet
+ */
+@WebServlet("/UserUpdateServlet")
+public class UserUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection users = null;
+		try {
+		request.setCharacterEncoding("utf-8");//←これを忘れると入力データが文字化けする
+		users = DBConnection.openConnection();
+		String id = request.getParameter("updateId");
+		String name = request.getParameter("updateName");
+		Statement state = users.createStatement();
+		state.executeUpdate("UPDATE user_table SET name='" + name + "' WHERE id ='" + id + "'");//←この部分は1行で入力する"
+		DBConnection.closeConnection(users, state);
+		response.sendRedirect("/select"); // UserSelectServletを呼び出す
+		} catch (SQLException e) {
+		e.printStackTrace();
+		}
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+	}
+
+}
